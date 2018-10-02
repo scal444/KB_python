2dedc574df7db6fb395fa76739fb46a12503248d
Kevin Boyd <kevin.boyd@uconn.edu>
2018-10-02 12:11:51 -0400

    added cart2spherical

--- coordinate_manipulation/transformations.py
+++ coordinate_manipulation/transformations.py
@@ -14,10 +14,35 @@ def cart2pol(cart_coords):
     '''
 
     # input validation
+    #if len(cart_coords.shape) is not 2 or cart_coords.shape[1] is not 3:
+    #    raise ValueError("dimension mismatch, expected (n_particles * 3), got {}".format(cart_coords.shape))
+    if len(cart_coords.shape) == 2:
+        x, y, z = cart_coords[:, 0], cart_coords[:, 1], cart_coords[:, 2]
+    elif len(cart_coords.shape) == 3:
+        x, y, z = cart_coords[:, :, 0], cart_coords[:, :, 1], cart_coords[:, :, 2]
+
+    theta    = np.arctan2(y, x)
+    rho  = np.sqrt(x**2 + y**2)
+    return (theta, rho, z)
+
+
+def cart2spherical(cart_coords):
+    ''' Converts cartesian to spherical coordinates.
+
+        Parameters -
+            cart_coords - n_parts * 3 array
+        returns
+            theta - n_parts, - range is -pi to pi
+            rho   - n_parts, radial coordinates
+            phi   - n_parts  - range is 0 to pi
+    '''
+
+    # input validation
     if len(cart_coords.shape) is not 2 or cart_coords.shape[1] is not 3:
         raise ValueError("dimension mismatch, expected (n_particles * 3), got {}".format(cart_coords.shape))
 
     x, y, z = cart_coords[:, 0], cart_coords[:, 1], cart_coords[:, 2]
     theta    = np.arctan2(y, x)
-    rho  = np.sqrt(x**2 + y**2)
-    return (theta, rho, z)
+    rho  = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    phi = np.arccos(z / rho)
+    return (theta, rho, phi)
