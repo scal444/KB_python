e62bc0ca6b19f50f64990de857b01e8497408572
Kevin Boyd <kevin.boyd@uconn.edu>
2019-02-19 17:31:15 -0500

    Added dihedral from vectors stuff

--- coordinate_manipulation/angles.py
+++ coordinate_manipulation/angles.py
@@ -1,12 +1,11 @@
 import numpy as np
-
 '''
     Functions for calculating angles, dihedrals planes
 '''
 
 
-def normalizeVector(vec, axis=None):
-
+def vectorNorm(vec, axis=None):
+    ''' Returns vector norm. Singleton in dimension along which the norm was made, so that numpy math can proceed'''
     vecShape = len(vec.shape)
     # do along last axis if none is given
     if not axis:
@@ -14,8 +13,45 @@ def normalizeVector(vec, axis=None):
 
     if axis >= vecShape:
         raise IndexError("Attempted to sum along axis {}, but the vector has shape {}".format(axis, vecShape))
-    return np.sqrt((vec ** 2).sum(axis=axis))
+    return np.expand_dims(np.sqrt((vec ** 2).sum(axis=axis)), axis)
 
 
-def normalVectorToPlane(vec1, vec2):
-    pass
+def angleFromVectors(v1, v2):
+    axis = len(v1.shape) - 1
+    nv1 = v1 / vectorNorm(v1)
+    nv2 = v2 / vectorNorm(v2)
+    return np.arccos(np.clip((nv1 * nv2).sum(axis=axis), -1.0, 1.0))
+
+
+def dihedralFromVectors(v1, v2, v3):
+    ''' Calculates the dihedral angle between 3 vectors, with v1, v2, v3 in sequence.
+        DOES NOT ACCOUNT FOR PBC
+
+        The math here is taken from Rahul on stackexchange
+        https://math.stackexchange.com/questions/47059/how-do-i-calculate-a-dihedral-angle-given-cartesian-coordinates
+    '''
+    axis = len(v1.shape) - 1
+
+    # normalize in case not done so yet
+    nv1 = v1 / vectorNorm(v1)
+    nv2 = v2 / vectorNorm(v2)
+    nv3 = v3 / vectorNorm(v3)
+
+    # calculate normal vectors to the planes defined by (v1, v2) and (v2, v3)
+    plane1NormalVec = np.cross(nv1, nv2)
+    plane2NormalVec = np.cross(nv2, nv3)
+
+    # calculate coordinates in an orthonormal frame
+    x = (plane1NormalVec * plane2NormalVec).sum(axis=axis)
+    y = (np.cross(plane1NormalVec, nv2) * plane2NormalVec).sum(axis=axis)
+    return np.arctan2(y, x)
+
+
+def dihedralFromPoints(p1, p2, p3, p4):
+    ''' Calculates the dihedral angle between 4 points with p1,p2,p3,p4 in sequence.
+        DOES NOT ACCOUNT FOR PBC
+        '''
+    v1 = p2 - p1
+    v2 = p3 - p2
+    v3 = p4 - p3
+    return dihedralFromVectors(v1, v2, v3)
--- coordinate_manipulation/tests/test_angles.py
+++ coordinate_manipulation/tests/test_angles.py
@@ -3,28 +3,60 @@ import unittest
 import KB_python.coordinate_manipulation.angles as angles
 
 
-class test_normalizeVector(unittest.TestCase):
+class test_vectorNorm(unittest.TestCase):
     def test_sumsCorrectDefaultAxis(self):
         array = np.zeros((3, 2, 4))
-        result = angles.normalizeVector(array)
-        self.assertEqual(result.shape, (3, 2))
+        result = angles.vectorNorm(array)
+        self.assertEqual(result.shape, (3, 2, 1))
 
     def test_sumsCorrectSpecifiedAxis(self):
         array = np.zeros((3, 2, 4))
-        result = angles.normalizeVector(array, 1)
-        self.assertEqual(result.shape, (3, 4))
+        result = angles.vectorNorm(array, 1)
+        self.assertEqual(result.shape, (3, 1, 4))
 
     def test_raisesIndexException(self):
         array = np.zeros(((3, 5, 2)))
         with self.assertRaises(IndexError):
-            angles.normalizeVector(array, 3)
+            angles.vectorNorm(array, 3)
 
     def test_producesCorrectOutput(self):
         array = np.array(((3, 4), (-3, 4), (4, -3)))
-        result = angles.normalizeVector(array)
+        result = angles.vectorNorm(array)
         self.assertAlmostEqual(result[0], 5)
         self.assertAlmostEqual(result[1], 5)
         self.assertAlmostEqual(result[2], 5)
+
+
+class test_anglesFromVectors(unittest.TestCase):
+
+    def test_produces_correct_output(self):
+        v1 = np.array((0, 0, 1))
+        v2 = np.array((0, 1, 0))
+        self.assertAlmostEqual(angles.angleFromVectors(v1, v2), np.pi / 2)
+        v2 = np.array((0, 0, -1))
+        self.assertAlmostEqual(angles.angleFromVectors(v1, v2), np.pi)
+        v2 = np.array((-2, 0, 0))
+        self.assertAlmostEqual(angles.angleFromVectors(v1, v2), np.pi / 2)
+
+
+class test_dihedralFromVectors(unittest.TestCase):
+
+    def test_producesCorrectOutput(self):
+        # simple unit case, 180 degrees
+        v1 = np.array((0, 1, 0))
+        v2 = np.array((1, 0, 0))
+        v3 = np.array((0, 1, 0))
+        self.assertAlmostEqual(angles.dihedralFromVectors(v1, v2, v3), np.pi)
+
+        # simple unit case, 0 degree
+        v3 = np.array((0, -1, 0))
+        self.assertAlmostEqual(angles.dihedralFromVectors(v1, v2, v3), 0)
+
+        # simple unit case, 90 degrees
+        v3 = np.array((0, 0, 1))
+        self.assertAlmostEqual(angles.dihedralFromVectors(v1, v2, v3), np.pi / 2)
+        v3 = np.array((0, 0, -1))
+        self.assertAlmostEqual(angles.dihedralFromVectors(v1, v2, v3), - np.pi / 2)
 
 
 if __name__ == '__main__':
